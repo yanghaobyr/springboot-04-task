842fae9f7e5ac15f9c5c831bd0bfd96f73ba5621
admin <949347292@qq.com>
2018-10-16 15:20:41 +0800

    Initial commit

---
+++ src/test/java/com/atguigu/task/Springboot04TaskApplicationTests.java
@@ -0,0 +1,56 @@
+package com.atguigu.task;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot04TaskApplicationTests {
+
+	@Autowired
+	JavaMailSenderImpl mailSender;
+
+	@Test
+	public void contextLoads() {
+		SimpleMailMessage message = new SimpleMailMessage();
+		//邮件设置
+		message.setSubject("通知-今晚开会");
+		message.setText("今晚7:30开会");
+
+		message.setTo("2469595543@qq.com");
+		message.setFrom("949347292@qq.com");//发送人
+
+		mailSender.send(message);
+	}
+
+	@Test
+	public void test02() throws  Exception{
+		//1、创建一个复杂的消息邮件
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+		//邮件设置
+		helper.setSubject("通知-今晚开会");
+		helper.setText("<b style='color:red'>今天 7:30 开会</b>",true);
+
+		helper.setTo("2469595543@qq.com");
+		helper.setFrom("949347292@qq.com");
+
+		//上传文件
+		helper.addAttachment("1.jpg",new File("C:\\Users\\admin\\Desktop\\827357321999894174.jpg"));
+		helper.addAttachment("Users.xls",new File("C:\\Users\\admin\\Desktop\\Users.xls"));
+
+		mailSender.send(mimeMessage);
+
+	}
+
+}
